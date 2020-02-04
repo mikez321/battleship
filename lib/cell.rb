2edcef4792d05d5f8095292287ffc610ee12b432
Mariana Cid <cidmariana5@gmail.com>
2020-02-04 13:57:36 -0700

    Some refactor to testing and render method complete for cell files.

--- lib/cell.rb
+++ lib/cell.rb
@@ -23,5 +23,22 @@ class Cell
 
   def fire_upon
     @fired_upon = true
+    if @ship != nil
+      @ship.hit
+    end
+  end
+
+  def render(ship = false)
+    if fired_upon? && empty?
+      "M"
+    elsif ship && !fired_upon? && !empty?
+      "S"
+    elsif fired_upon? && !empty? && !@ship.sunk?
+      "H"
+    elsif fired_upon? && !empty? && @ship.sunk?
+      "X"
+    else empty?
+      "."
+    end
   end
 end
