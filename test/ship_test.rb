07da74a311d04f98b765598036241ebbdfa728ba
Mariana Cid <cidmariana5@gmail.com>
2020-02-03 22:00:41 -0700

    Put all attributes in one test instead of seperate tests.

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -10,15 +10,10 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
-  def test_it_has_a_name
+  def test_it_has_attributes
     cruiser = Ship.new("Cruiser", 3)
 
     assert_equal "Cruiser", cruiser.name
-  end
-
-  def test_it_has_a_length
-    cruiser = Ship.new("Cruiser", 3)
-
     assert_equal 3, cruiser.length
   end
 
