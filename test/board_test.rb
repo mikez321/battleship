969377a5f18a9760bcb0cf82ee3a8c8d7c6706ae
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-04 12:29:11 -0700

    Add test for valid coordinates

---
+++ test/board_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_can_test_for_valid_coordinates
+    board = Board.new
+    board.cells
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
+end
