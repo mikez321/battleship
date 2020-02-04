ed2a388053e947218103da23b4edb356c800b8cb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-04 13:04:26 -0700

    Create valid placement tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -23,4 +23,19 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?("A22")
   end
 
+  def test_it_can_test_for_valid_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.valid_placement?(submarine, ["A2", "A3", "A4"])
+
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  end
+
 end
