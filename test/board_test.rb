1d6ffe4f4c9c3209a6a10aee9fffc582fa60906d
Mariana Cid <cidmariana5@gmail.com>
2020-02-05 15:14:32 -0700

    Refactored refute/assert to assert_equal true/false

--- test/board_test.rb
+++ test/board_test.rb
@@ -20,9 +20,9 @@ class BoardTest < Minitest::Test
   def test_it_has_valid_coordiantes
     board = Board.new
     assert_equal true, board.valid_coordinate?("A1")
-    assert board.valid_coordinate?("D4")
+    assert_equal true, board.valid_coordinate?("D4")
     assert_equal false, board.valid_coordinate?("A5")
-    refute board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("E1")
   end
 
   def test_it_has_valid_placement
@@ -30,18 +30,18 @@ class BoardTest < Minitest::Test
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     # first two are testing to make sure the valid_placement is accoring to ships
-    refute board.valid_placement?(cruiser, ["A1", "A2"])
-    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    # next for are to make sure the coordiantes are consise
-    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    refute board.valid_placement?(submarine, ["A1", "C1"])
-    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    refute board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    # next for are to make sure the coordiantes are
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
     # next two are to make sure they arent diagonal
-    refute board.valid_placement?(cruiser, ["A1", "B2", "C3"])
-    refute board.valid_placement?(submarine, ["C2", "D3"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
     # just to double check all past test pass
-    assert board.valid_placement?(submarine, ["A1", "A2"])
-    assert board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 end
