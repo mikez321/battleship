6f1ff24833209bd8cc2d70983862dc9574a04c07
Mariana Cid <cidmariana5@gmail.com>
2020-02-05 13:18:58 -0700

    First test created to confirm board exists.

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,5 +5,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class BoardTest < Minitest::Test
-  
+
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
 end
