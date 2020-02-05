32fe4ae450746e59d6674d1599f7ac9fb71b215e
Mariana Cid <cidmariana5@gmail.com>
2020-02-05 13:42:30 -0700

    Using boolean logic to write test for valid coordinate and valid placement

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,4 +10,29 @@ class BoardTest < Minitest::Test
     board = Board.new
     assert_instance_of Board, board
   end
+
+  def test_it_has_cells
+    board = Board.new
+    assert_equal Hash, board.cells
+    assert_equal 16, board.cells.length
+  end
+
+  def test_it_has_valid_coordiantes
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("A1")
+    assert board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    refute board.valid_coordinate?("E1")
+  end
+
+  def test_it_has_valid_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    refute board.valid_placement?(cruiser, ["A1", "A2"])
+    refute board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    refute board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    refute board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    refute board.valid_placement?(submarine, ["C1", "B1"])
+  end
 end
