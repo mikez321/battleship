d15fbbd9f11e2b4f5a1fe51b281564460c8bfc67
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-05 19:07:53 -0700

    Create valid coordinates method

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,20 @@
 class Board
   def initialize
-    @cells = ["A1","A2","A3","A4",
+    @cell_names = ["A1","A2","A3","A4",
               "B1","B2","B3","B4",
               "C1","C2","C3","C4",
               "D1","D2","D3","D4"]
+    @board_of_cells = Hash.new
   end
 
   def cells
-    board_of_cells = Hash.new
-    @cells.each do |cell_name|
-      board_of_cells[cell_name] = Cell.new(cell_name)
+    @cell_names.each do |name|
+      @board_of_cells[name] = Cell.new(name)
     end
-    board_of_cells
+    @board_of_cells
+  end
+
+  def valid_coordinate?(coordinate)
+    cells.keys.include?(coordinate)
   end
 end
