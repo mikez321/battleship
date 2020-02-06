b032b5f1a48e5efb04fcc6558c5dad0f25bd6143
Mariana Cid <cidmariana5@gmail.com>
2020-02-06 14:46:18 -0700

    Render method refactored, and completed with tests.

--- test/board_test.rb
+++ test/board_test.rb
@@ -71,6 +71,7 @@ class BoardTest < Minitest::Test
 
     assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
     board.place(cruiser, ["A1", "A2", "A3"])
+    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
     assert_equal "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", board.render(true)
   end
 end
