36f4d8d8a5aa6fe5cb293808665304c5905a3e38
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-07 14:48:04 -0700

    Edited results of board test since validators now accept backwards coordinate inputs

--- test/board_test.rb
+++ test/board_test.rb
@@ -42,8 +42,8 @@ class BoardTest < Minitest::Test
     # # next four are to make sure the coordiantes are consecutive
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
-    # assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    # assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal true, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal true, board.valid_placement?(submarine, ["C1", "B1"])
     # next two are to make sure they arent diagonal
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
