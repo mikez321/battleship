237541ddfdfdd525e032d285bb7791d52f69803e
Mariana Cid <cidmariana5@gmail.com>
2020-02-06 17:20:11 -0700

    Start of game play interation with user.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,31 @@
 class Game
-  
+
+  def initialize
+    @player_board = Board.new
+    @computer_board = Board.new
+    @cruiser = Ship.new("Crusier", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @start_option = " "
+  end
+
+  def welcome
+    p "Welcome to BATTLESHIP! \n" +
+      "Enter p to play. Enter q to quit"
+
+      @start_option = gets.chomp.downcase
+  end
+
+  def setup
+    if @start_option == "p"
+      p "Geat! Lets play!"
+
+      p "==============PLAYER BOARD=============="
+      p @player_board.render(true)
+
+      p "=============COMPUTER BOARD============="
+      p @computer_board.render
+    else @start_option == "q"
+      p "Boo, you suck!"
+    end
+  end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -2,14 +2,54 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/game'
-require 'minitest/autorun'
-require 'minitest/pride'
-
-class GameTest < Minitest::Test
+# require 'minitest/autorun'
+# require 'minitest/pride'
 
 
-  def test_it_exists
-    game = Game.new
-    assert_instance_of Game, game
-  end
-end
+game = Game.new
+game.welcome
+game.setup
+
+
+# class GameTest < Minitest::Test
+#
+#
+#   def test_it_exists
+#     game = Game.new
+#     assert_instance_of Game, game
+#   end
+#
+#   def test_welcome
+#     game = Game.new
+#     assert_equal game.welcome
+  # end
+
+  # def test_game_can_start
+  #   game = Game.new
+  #   start_option = "p"
+  #
+  #   assert_equal "Heck yea!", game.game_start(start_option)
+  #   start_option = "q"
+  #   assert_equal "Bye Bye", game.game_start(start_option)
+  #
+  # end
+
+  # def test_it_has_welcome
+  #   game = Game.new
+  #   expected =  "Welcome to BATTLESHIP"
+  #               "Enter p to play. Enter q to quit."
+  #   assert_equal expected, game.welcome()
+  # end
+  #
+  # def test_when_game_starts_a_board_is_created
+  #   board = Board.new
+  #
+  #   assert_instance_of Board, board
+  # end
+  #
+  # def test_new_boad_has_no_ships_placed
+  #   board = Board.new
+  #
+  #
+  # end
+# end
