e3a14d26d0c895384a5dfec8b28540e4f58a9381
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-07 10:48:05 -0700

    Add second ship placement and begin computer ship placement

--- lib/game.rb
+++ lib/game.rb
@@ -20,16 +20,28 @@ class Game
 
     if start_option == "p"
       puts "Geat! Lets play!"
-
-      puts "==============PLAYER BOARD=============="
-      puts @player_board.render
-      player_place_ships
+      place_computer_ships
+      # puts "==============PLAYER BOARD=============="
+      # puts @player_board.render
+      # player_place_ship1
     else
       puts  "Boo, you suck!"
     end
   end
 
-  def player_place_ships
+  def place_computer_ships
+    require "Pry"; binding.pry
+    cells.keys.shuffle.first
+    rand_coordinates = []
+
+    2.times do
+      rand_coordinates << cells.keys.shuffle.first
+      rand_coordinates
+    end
+  end
+
+  def player_place_ship1
+    require "Pry"; binding.pry
     puts "Your ships are: "
     puts "The #{@player_cruiser.name} which will take up #{@player_cruiser.length} spaces."
     puts "The #{@player_submarine.name} which will take up #{@player_submarine.length} spaces."
@@ -37,18 +49,55 @@ class Game
     puts "Enter the coordinates of where you want to place the #{@player_submarine.name}:"
     print "> "
     ship1_coordinates = gets.chomp.delete(" ").split(",")
-    if @player_board.valid_placement?(@player_submarine, ship1_coordinates) && ship1_coordinates.each { |coord| @player_board.valid_coordinate?(coord) }
-      ship1_coordinates.each do |coord|
-        @player_board.cells[coord].place_ship(@player_submarine)
-      end
-      puts "Your board now looks like this:"
-      puts "==============PLAYER BOARD=============="
-      puts @player_board.render(true)
-    else
+    until @player_board.valid_placement?(@player_submarine, ship1_coordinates) && ship1_coordinates.each { |coord| @player_board.valid_coordinate?(coord) }
       puts "Enter new coordinates, the ones you entered are invalid."
+      ship1_coordinates = gets.chomp.delete(" ").split(",")
+    end
+    puts "Finally, #{ship1_coordinates} are valid!"
+
+    ship1_coordinates.each do |coord|
+      @player_board.cells[coord].place_ship(@player_submarine)
+    end
+    puts "Your board now looks like this:"
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+      puts "Press n to move on to the next ship or u to replace the #{@player_submarine.name}."
+      option_2 = gets.chomp
+      if option_2 == "n"
+        player_place_ship2
+      elsif
+        option_2 == "u"
+        player_place_ship1
+      end
+  end
+
+  def player_place_ship2
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+
+    puts "Now place your #{@player_cruiser.name} which will take up #{@player_cruiser.length} spaces."
+    print "> "
+    ship2_coordinates = gets.chomp.delete(" ").split(",")
+    until @player_board.valid_placement?(@player_cruiser, ship2_coordinates) && ship2_coordinates.each { |coord| @player_board.valid_coordinate?(coord) }
+      puts "Enter new coordinates, the ones you entered are invalid."
+      ship2_coordinates = gets.chomp.delete(" ").split(",")
+    end
+    puts "Finally, #{ship2_coordinates} are valid!"
+
+    ship2_coordinates.each do |coord|
+      @player_board.cells[coord].place_ship(@player_cruiser)
+    end
+    puts "Your board now looks like this:"
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
+    puts "Press y to begin or u to replace the #{@player_cruiser.name}."
+    option_3 = gets.chomp
+    if option_3 == "y"
+      turn
+    elsif
+      option_3 == "u"
+      player_place_ship2
     end
 
   end
-
-
 end
