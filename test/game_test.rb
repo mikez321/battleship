23460ad53b80382c14de578eb5df336025c5fd9f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-06 17:32:21 -0700

    Create game test

---
+++ test/game_test.rb
@@ -0,0 +1,10 @@
+require './lib/ship'
+require './lib/ship'
+require './lib/ship'
+require './lib/ship'
+require 'minitest/autorun'
+requier 'minitest/pride'
+
+class GameTest < Minitest::Test
+
+end
