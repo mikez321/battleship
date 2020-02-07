7c03b5c78dac653ecf9c1cd75566b096da0a9f39
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-06 20:16:03 -0700

    Add some basic functions as an outline

--- lib/game.rb
+++ lib/game.rb
@@ -5,31 +5,49 @@ require './lib/board'
 class Game
 
   def initialize
-    @player_board = Board.new
-    @computer_board = Board.new
+    @board = Board.new
     @cruiser = Ship.new("Crusier", 3)
     @submarine = Ship.new("Submarine", 2)
-    @start_option = " "
   end
 
-  def welcome
-    p "Welcome to BATTLESHIP! \n" +
-      "Enter p to play. Enter q to quit"
+  def start
+    puts "Welcome to BATTLESHIP!"
+    puts "Enter p to play. Enter q to quit"
+    start_option = gets.chomp
 
-      @start_option = gets.chomp.downcase
-  end
+    if start_option == "p"
+      puts "Geat! Lets play!"
 
-  def setup
-    if @start_option == "p"
-      p "Geat! Lets play!"
-
-      p "==============PLAYER BOARD=============="
-      p @player_board.render(true)
-
-      p "=============COMPUTER BOARD============="
-      p @computer_board.render
-    else @start_option == "q"
-      p "Boo, you suck!"
+      puts "==============PLAYER BOARD=============="
+      puts @board.render
+      place_ships
+    else
+      puts  "Boo, you suck!"
     end
   end
+
+  def place_ships
+    puts "Your ships are: "
+    puts "The #{@cruiser.name} which will take up #{@cruiser.length} spaces."
+    puts "The #{@submarine.name} which will take up #{@submarine.length} spaces."
+    puts "\nYou must place your ships on the board.  Spaces must be consecutive\nand obviously the ships have to be fully on the board."
+    puts "Enter the coordinates of where you want to place the #{@submarine.name}:"
+    print "> "
+    ship1_coordinates = gets.chomp
+    ship1_coordinates.split(",").each do |coord|
+      @board.cells[coord.delete(" ")].place_ship(@submarine)
+    end
+    # if
+    #   @board.valid_coordinate?(ship1_coordinates)
+    #   @board.cells[ship1_coordinates].place_ship(@submarine)
+    # else
+    #   "Enter new coordinates, the ones you entered are invalid."
+    # end
+    puts "Your board now looks like this:"
+    puts "==============PLAYER BOARD=============="
+    puts @board.render(true)
+
+  end
+
+
 end
