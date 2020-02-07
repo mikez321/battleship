f6ee0abb9091459e324301eb36826103e56e4115
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-06 21:11:24 -0700

    Create placement validators for ship placement and vars for computer player

--- lib/game.rb
+++ lib/game.rb
@@ -5,9 +5,12 @@ require './lib/board'
 class Game
 
   def initialize
-    @board = Board.new
-    @cruiser = Ship.new("Crusier", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @player_board = Board.new
+    @player_cruiser = Ship.new("Crusier", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_board = Board.new
+    @coputer_cruiser = Ship.new("Crusier", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
   end
 
   def start
@@ -19,32 +22,31 @@ class Game
       puts "Geat! Lets play!"
 
       puts "==============PLAYER BOARD=============="
-      puts @board.render
-      place_ships
+      puts @player_board.render
+      player_place_ships
     else
       puts  "Boo, you suck!"
     end
   end
 
-  def place_ships
+  def player_place_ships
     puts "Your ships are: "
-    puts "The #{@cruiser.name} which will take up #{@cruiser.length} spaces."
-    puts "The #{@submarine.name} which will take up #{@submarine.length} spaces."
+    puts "The #{@player_cruiser.name} which will take up #{@player_cruiser.length} spaces."
+    puts "The #{@player_submarine.name} which will take up #{@player_submarine.length} spaces."
     puts "\nYou must place your ships on the board.  Spaces must be consecutive\nand obviously the ships have to be fully on the board."
-    puts "Enter the coordinates of where you want to place the #{@submarine.name}:"
+    puts "Enter the coordinates of where you want to place the #{@player_submarine.name}:"
     print "> "
-    ship1_coordinates = gets.chomp
-    if
-      @board.valid_placement?(@submarine, ship1_coordinates.delete(" ").split(","))
-      ship1_coordinates.split(",").each do |coord|
-        @board.cells[coord.delete(" ")].place_ship(@submarine)
+    ship1_coordinates = gets.chomp.delete(" ").split(",")
+    if @player_board.valid_placement?(@player_submarine, ship1_coordinates) && ship1_coordinates.each { |coord| @player_board.valid_coordinate?(coord) }
+      ship1_coordinates.each do |coord|
+        @player_board.cells[coord].place_ship(@player_submarine)
       end
+      puts "Your board now looks like this:"
+      puts "==============PLAYER BOARD=============="
+      puts @player_board.render(true)
     else
-      "Enter new coordinates, the ones you entered are invalid."
+      puts "Enter new coordinates, the ones you entered are invalid."
     end
-    puts "Your board now looks like this:"
-    puts "==============PLAYER BOARD=============="
-    puts @board.render(true)
 
   end
 
