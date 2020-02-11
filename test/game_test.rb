6f0442b6765d2caf56969c000c7b6df2486029db
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-10 20:22:21 -0700

    Create tests for applicable game methods

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,71 +1,60 @@
 require './lib/ship'
-<<<<<<< HEAD
-require './lib/ship'
-require './lib/ship'
-require './lib/ship'
-require 'minitest/autorun'
-requier 'minitest/pride'
-
-class GameTest < Minitest::Test
-
-end
-=======
 require './lib/cell'
 require './lib/board'
 require './lib/game'
-# require 'minitest/autorun'
-# require 'minitest/pride'
-@player_board = Board.new
-@computer_board = Board.new
-@cruiser = Ship.new("Crusier", 3)
-@submarine = Ship.new("Submarine", 2)
-@start_option = " "
+require 'minitest/autorun'
+require 'minitest/pride'
 
-game = Game.new
-game.welcome
-game.setup
-require "pry"; binding.pry
+class GameTest < Minitest::Test
 
-# class GameTest < Minitest::Test
-#
-#
-#   def test_it_exists
-#     game = Game.new
-#     assert_instance_of Game, game
-#   end
-#
-#   def test_welcome
-#     game = Game.new
-#     assert_equal game.welcome
-  # end
+  def test_it_exists
+    game = Game.new
 
-  # def test_game_can_start
-  #   game = Game.new
-  #   start_option = "p"
-  #
-  #   assert_equal "Heck yea!", game.game_start(start_option)
-  #   start_option = "q"
-  #   assert_equal "Bye Bye", game.game_start(start_option)
-  #
-  # end
+    assert_instance_of Game, game
+  end
 
-  # def test_it_has_welcome
-  #   game = Game.new
-  #   expected =  "Welcome to BATTLESHIP"
-  #               "Enter p to play. Enter q to quit."
-  #   assert_equal expected, game.welcome()
-  # end
-  #
-  # def test_when_game_starts_a_board_is_created
-  #   board = Board.new
-  #
-  #   assert_instance_of Board, board
-  # end
-  #
-  # def test_new_boad_has_no_ships_placed
-  #   board = Board.new
-  #
-  #
-  # end
-# end
->>>>>>> d74dd1c25dd7d23bf04e058c43c515fab5aff295
+  def test_it_has_ships_and_boards
+    game = Game.new
+
+    assert_instance_of Board, game.player_board
+    assert_instance_of Ship, game.player_cruiser
+    assert_instance_of Ship, game.player_submarine
+    assert_instance_of Board, game.computer_board
+    assert_instance_of Ship, game.computer_cruiser
+    assert_instance_of Ship, game.computer_submarine
+  end
+
+  def test_it_has_a_banner
+    skip
+
+  end
+
+  def test_it_can_place_its_own_ships
+    game = Game.new
+
+    assert_equal false, game.computer_cruiser.placed?
+    assert_equal false, game.computer_submarine.placed?
+
+    game.place_computer_ships
+
+    assert_equal true, game.computer_cruiser.placed?
+    assert_equal true, game.computer_submarine.placed?
+  end
+
+  def test_it_can_display_a_header
+  skip
+  end
+
+  def test_it_can_take_a_variety_of_inputs_for_coordinates
+    game = Game.new
+
+    assert_equal ["A1"], game.normalize_input_coordinates("a1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("a1 b1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("a1, b1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("A1 b1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("a1 B1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("A1 B1")
+    assert_equal ["A1", "B1"], game.normalize_input_coordinates("A1, B1")
+  end
+
+end
